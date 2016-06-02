d42f85b1538fee936dd9f7edd933bcb3bc3387fc
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-06-02 18:18:59 +0930

    Add test for parsePageString

--- misc_test.go
+++ misc_test.go
@@ -117,3 +117,41 @@ func BenchmarkIoCopy10m(b *testing.B) {
 func BenchmarkIoCopy100m(b *testing.B) {
 	benchmarkCopyFunc(b, io.Copy, 100*1024*1024)
 }
+
+func TestParsePageString(t *testing.T) {
+	if n, redirect, err := parsePageString(""); n != 1 || redirect || err != nil {
+		t.Errorf(`parsePageString("") = (%d, %t, %v), expected (1, false, <nil>)`, n, redirect, err)
+	}
+
+	if n, redirect, err := parsePageString("1"); n != 1 || !redirect || err != nil {
+		t.Errorf(`parsePageString("1") = (%d, %t, %v), expected (1, true, <nil>)`, n, redirect, err)
+	}
+
+	if n, redirect, err := parsePageString("2"); n != 2 || redirect || err != nil {
+		t.Errorf(`parsePageString("2") = (%d, %t, %v), expected (2, false, <nil>)`, n, redirect, err)
+	}
+
+	if n, redirect, err := parsePageString("5"); n != 5 || redirect || err != nil {
+		t.Errorf(`parsePageString("5") = (%d, %t, %v), expected (5, false, <nil>)`, n, redirect, err)
+	}
+
+	if n, redirect, err := parsePageString("10"); n != 10 || redirect || err != nil {
+		t.Errorf(`parsePageString("10") = (%d, %t, %v), expected (10, false, <nil>)`, n, redirect, err)
+	}
+
+	if n, redirect, err := parsePageString("1000"); n != 1000 || redirect || err != nil {
+		t.Errorf(`parsePageString("1000") = (%d, %t, %v), expected (1000, false, <nil>)`, n, redirect, err)
+	}
+
+	if _, _, err := parsePageString("0"); err == nil {
+		t.Error(`parsePageString("0") = (_, _, <nil>), expected (_, _, ...)`)
+	}
+
+	if _, _, err := parsePageString("-1"); err == nil {
+		t.Error(`parsePageString("-1") = (_, _, <nil>), expected (_, _, ...)`)
+	}
+
+	if _, _, err := parsePageString("-100"); err == nil {
+		t.Error(`parsePageString("-100") = (_, _, <nil>), expected (_, _, ...)`)
+	}
+}
