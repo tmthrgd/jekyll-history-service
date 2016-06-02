3762eb164ec040d3fe0e7877fa3e70ac72870a61
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-06-02 18:42:39 +0930

    Run go fmt

--- goto_test.go
+++ goto_test.go
@@ -12,9 +12,9 @@ import (
 	"testing"
 )
 
-type fakeResponseWriter struct{
+type fakeResponseWriter struct {
 	Headers http.Header
-	Code int
+	Code    int
 }
 
 func (rw *fakeResponseWriter) Header() http.Header {
@@ -32,59 +32,59 @@ func (rw *fakeResponseWriter) WriteHeader(code int) {
 func TestGotoHandler(t *testing.T) {
 	for url, expect := range map[string]string{
 		// no url
-		"http://example.com/": "http://example.com/",
-		"http://example.com/?url=": "http://example.com/",
+		"http://example.com/":            "http://example.com/",
+		"http://example.com/?url=":       "http://example.com/",
 		"http://example.com/?url=%3Fa=b": "http://example.com/",
-		"http://example.com/?url=%23a": "http://example.com/",
+		"http://example.com/?url=%23a":   "http://example.com/",
 
 		// no user, reop or commit
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2F": "http://example.com/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2F":       "http://example.com/",
 		"http://example.com/?url=https%3A%2F%2Fgithub.com%2F%3Fa=b": "http://example.com/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2F%23a": "http://example.com/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2F%23a":   "http://example.com/",
 
 		// user
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample": "http://example.com/u/example/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2F": "http://example.com/u/example/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample": "http://example.com/u/example/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2F": "http://example.com/u/example/",
-		"http://example.com/?url=github.com%2Fexample": "http://example.com/u/example/",
-		"http://example.com/?url=github.com%2Fexample%2F": "http://example.com/u/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample":       "http://example.com/u/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2F":    "http://example.com/u/example/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample":        "http://example.com/u/example/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2F":     "http://example.com/u/example/",
+		"http://example.com/?url=github.com%2Fexample":                     "http://example.com/u/example/",
+		"http://example.com/?url=github.com%2Fexample%2F":                  "http://example.com/u/example/",
 		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%3Fa=b": "http://example.com/u/example/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%23a": "http://example.com/u/example/",
-		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample": "http://example.com/u/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%23a":   "http://example.com/u/example/",
+		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample":       "http://example.com/u/example/",
 
 		// user + repo
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2F": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2F": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=github.com%2Fexample%2Fexample": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=github.com%2Fexample%2Fexample%2F": "http://example.com/u/example/r/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample":       "http://example.com/u/example/r/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2F":    "http://example.com/u/example/r/example/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample":        "http://example.com/u/example/r/example/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2F":     "http://example.com/u/example/r/example/",
+		"http://example.com/?url=github.com%2Fexample%2Fexample":                     "http://example.com/u/example/r/example/",
+		"http://example.com/?url=github.com%2Fexample%2Fexample%2F":                  "http://example.com/u/example/r/example/",
 		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%3Fa=b": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%23a": "http://example.com/u/example/r/example/",
-		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample%2Fexample%2F": "http://example.com/u/example/r/example/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%23a":   "http://example.com/u/example/r/example/",
+		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample%2Fexample%2F":    "http://example.com/u/example/r/example/",
 
 		// user + repo + commit
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=github.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=github.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290":       "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F":    "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290":        "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F":     "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=github.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290":                     "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=github.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%2F":                  "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
 		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%3Fa=b": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%23a": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
-		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fgit%2Fgit%2FcOmMiT%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290": "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fgit%2Fgit%2Fcommit%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290%23a":   "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
+		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fgit%2Fgit%2FcOmMiT%2Fe83c5163316f89bfbde7d9ab23ca2e25604af290":       "http://example.com/u/git/r/git/c/e83c5163316f89bfbde7d9ab23ca2e25604af290/",
 
 		// user + repo + tree
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=github.com%2Fexample%2Fexample%2Ftree%2Fmaster": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=github.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F": "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster":       "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F":    "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster":        "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=http%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F":     "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=github.com%2Fexample%2Fexample%2Ftree%2Fmaster":                     "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=github.com%2Fexample%2Fexample%2Ftree%2Fmaster%2F":                  "http://example.com/u/example/r/example/t/master/",
 		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%3Fa=b": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%23a": "http://example.com/u/example/r/example/t/master/",
-		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample%2Fexample%2FtReE%2Fmaster%2F": "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=https%3A%2F%2Fgithub.com%2Fexample%2Fexample%2Ftree%2Fmaster%23a":   "http://example.com/u/example/r/example/t/master/",
+		"http://example.com/?url=https%3A%2F%2FGiThUb.cOm%2Fexample%2Fexample%2FtReE%2Fmaster%2F":    "http://example.com/u/example/r/example/t/master/",
 	} {
 		req, err := http.NewRequest(http.MethodGet, url, nil)
 		if err != nil {
--- templates_test.go
+++ templates_test.go
@@ -26,13 +26,13 @@ func TestTemplateFuncs(t *testing.T) {
 func TestAssetPath(t *testing.T) {
 	for name, expect := range map[string]string{
 		// internal
-		"a": "/assets/a",
+		"a":        "/assets/a",
 		"test.ext": "/assets/test.ext",
 
 		// external
-		"http://example.com/test.ext": "http://example.com/test.ext",
+		"http://example.com/test.ext":  "http://example.com/test.ext",
 		"https://example.com/test.ext": "https://example.com/test.ext",
-		"//example.com/test.ext": "//example.com/test.ext",
+		"//example.com/test.ext":       "//example.com/test.ext",
 	} {
 		if path := assetPath(name); path != expect {
 			t.Errorf("unexpected path for %s, expected %s, got %s", name, expect, path)
@@ -42,18 +42,18 @@ func TestAssetPath(t *testing.T) {
 
 func TestHTML5Attr(t *testing.T) {
 	for name, expect := range map[string]string{
-		"value": "value",
-		"%test$": "%test$",
-		"one and two": `"one and two"`,
+		"value":         "value",
+		"%test$":        "%test$",
+		"one and two":   `"one and two"`,
 		"one\tand\ttwo": `"` + "one\tand\ttwo" + `"`,
 		"one\nand\ntwo": `"` + "one\nand\ntwo" + `"`,
 		"one\fand\ftwo": `"` + "one\fand\ftwo" + `"`,
 		"one\rand\rtwo": `"` + "one\rand\rtwo" + `"`,
-		`one"and"two`: `"one&#34;and&#34;two"`,
-		"one'and'two": `"one&#39;and&#39;two"`,
-		"one`and`two": `"` + "one`and`two" + `"`,
-		"a=b": `"a=b"`,
-		"one<and>two": `"one&lt;and&gt;two"`,
+		`one"and"two`:   `"one&#34;and&#34;two"`,
+		"one'and'two":   `"one&#39;and&#39;two"`,
+		"one`and`two":   `"` + "one`and`two" + `"`,
+		"a=b":           `"a=b"`,
+		"one<and>two":   `"one&lt;and&gt;two"`,
 	} {
 		if attr := html5Attr(name); attr != expect {
 			t.Errorf("unexpected value for %q, expected %q, got %q", name, expect, attr)
