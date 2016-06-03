0e11ff81aa93d1997acdee5b31cbacba18c0bea8
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-06-03 20:45:06 +0930

    Use go-github's built in unathenticated transport

--- github-client.go
+++ github-client.go
@@ -7,37 +7,11 @@ package main
 
 import (
 	"errors"
-	"net/http"
 	"os"
 
 	"github.com/google/go-github/github"
 	"github.com/gregjones/httpcache"
 )
-
-type githubAuth struct {
-	ID     string
-	Secret string
-
-	Transport http.RoundTripper
-}
-
-func (ga githubAuth) RoundTrip(req *http.Request) (*http.Response, error) {
-	newReq := *req
-	url := *req.URL
-	q := req.URL.Query()
-
-	q.Set("client_id", ga.ID)
-	q.Set("client_secret", ga.Secret)
-
-	url.RawQuery = q.Encode()
-	newReq.URL = &url
-
-	if ga.Transport != nil {
-		return ga.Transport.RoundTrip(&newReq)
-	}
-
-	return http.DefaultTransport.RoundTrip(&newReq)
-}
 
 func getGithubClient() (*github.Client, error) {
 	transport := httpcache.NewMemoryCacheTransport()
@@ -45,9 +19,9 @@ func getGithubClient() (*github.Client, error) {
 
 	id := os.Getenv("GITHUB_CLIENT_ID")
 	if secret := os.Getenv("GITHUB_CLIENT_SECRET"); len(id) != 0 && len(secret) != 0 {
-		transport.Transport = &githubAuth{
-			ID:     id,
-			Secret: secret,
+		transport.Transport = &github.UnauthenticatedRateLimitedTransport{
+			ClientID:     id,
+			ClientSecret: secret,
 		}
 	} else if len(id) != 0 || len(secret) != 0 {
 		return nil, errors.New("both GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET must be set")
